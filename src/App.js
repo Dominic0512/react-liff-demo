9284b3046688ab62d4e83ee5dbf3629c079de413
Dominic Lee <dominiclee0512@gmail.com>
2020-09-21 15:12:20 +0800

    feat: implement basic seperate enable permission and add friend flow.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import liff from '@line/liff'
+
+const LIFF_ID = '1654949300-KRyrEX5D'
+const LINE_ADD_FRIEND_URI = 'https://line.me/R/ti/p/@685cokrk'
 
 function App() {
+  const [isReady, setIsReady] = React.useState(false)
+  const [isSignUp, setIsSignUp] = React.useState(false)
+  const [friendShip, setFriendShip] = React.useState(false)
+  
+  const openAddFriendModal = React.useCallback(() => {
+    if (!isReady) return;
+    liff.openWindow({ url: LINE_ADD_FRIEND_URI })
+  }, [isReady])
+
+  React.useEffect(() => {
+    (async () => {
+      try {
+        await liff.init({ liffId: LIFF_ID });
+        const { friendFlag } = await liff.getFriendship()
+        setFriendShip(friendFlag)
+        setIsReady(true)
+      } catch (error) {
+        alert(error.code, error.message);
+      }
+    })();
+  }, [])
+
+  React.useEffect(() => {
+    if (!isReady) return;
+    if (isSignUp && !friendShip) {
+      openAddFriendModal()
+    }
+  }, [isReady, isSignUp, friendShip, openAddFriendModal])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {isReady ? (
+        <>
+          <div>
+            {`Friendship: ${friendShip}`}
+          </div>
+          <div>
+            <button onClick={() => setIsSignUp(true)}>完成註冊</button>
+            {
+              !friendShip && (
+                <button onClick={() => openAddFriendModal()}>加入好友</button>
+              )
+            }
+          </div>
+        </>
+      ): (<>Loading...</>)}
     </div>
   );
 }
